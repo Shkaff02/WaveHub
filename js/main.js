da8114280e5723c80a05ed4a0c1ee4cf57a50423
Shkaff02 <78539412+Shkaff02@users.noreply.github.com>
2021-11-01 16:21:17 +0200

    WaveHub

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+
+  window.onload = function(){ 
+    document.getElementById('go').onclick = function() {
+        window.location.href = 'calendar.html';
+      };
+    
+      document.getElementById('dan').onclick = function() {
+        window.location.href = 'about-us.html';
+      };
+
+      document.getElementById('logotype').onclick = function() {
+        window.location.href = 'index.html';
+      };
+
+      document.getElementById('inst').onclick = function() {
+        window.location.href = 'https://instagram.com/wavehub.ro?utm_medium=copy_link';
+      };
+};
